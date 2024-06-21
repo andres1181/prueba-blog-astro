e5d7fa80c6197356747b26fd88d8e9889eaf9e1e
Andres <andreslozano1118@gmail.com>
2024-06-21 08:25:37 -0500

    Final tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site:  'https://prueba-blog-astro.netlify.app'
+  site: 'https://prueba-blog-astro.netlify.app',
+  integrations: [preact()]
 });
