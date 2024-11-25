604c4dff3482e25c47b9532cc480c74cf39d65fb
Andres <andreslozano1118@gmail.com>
2024-11-25 12:39:03 -0500

    Decapcms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-
-import preact from "@astrojs/preact";
+import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://prueba-blog-astro.netlify.app',
   integrations: [preact()]
-});
+})
